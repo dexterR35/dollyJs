a4f698b24150763bbcc93a2ab85b00cd5f5aa5a0
dexterR35 <pusaw202@gmail.com>
2024-01-04 18:21:26 +0200

    add dollyJs

--- index.js
+++ index.js
@@ -3,11 +3,10 @@
 
   function init() {
     /* INIT DOLLY */
-
     let dolly = new Dolly(document.getElementById("dolly-container"), {
       ease: 0.1, // animation ease
       perspective: 3000, // container perspective
-      distance: 5000, // distance between layers
+      distance: 9000, // distance between layers
       fullpage: true, // enable one scroll to move throgh layers
       delay: 1000, // if lockScroll its true - set delay betwen each scroll (deafult 1500 ms)
       duration: 800, // animation duration - for anchors and full page transitions (default 800 ms)
@@ -15,7 +14,7 @@
       activeMenu: true, // add class "active-anchor" to anchor
       onlyAnchors: false, // only anchors navigation
       bodyClass: true, // when layer is active add to body a class with the layer's id value - ex. 'on-layer1'
-      threshold: 30, // change index with N° pixel in advance
+      threshold: 10, // change index with N° pixel in advance
       //   activeMenu: true // gave class active to menu voices
       onReady: function (index) {
         console.log(index, "onChange");
@@ -34,15 +33,6 @@
         /* CALLED WHEN WINDOW IT'S RESIZED */
       },
     });
-
-    // stopDolly();
-    // startDolly();
-
-    // blockScroll(boolean);
-
-    // getIndex();
-    // getScroll();
-    // moveToIndex(index, duration);
   }
 
   document.addEventListener("DOMContentLoaded", function (event) {
@@ -50,38 +40,8 @@
   });
 })();
 
-$(".testt").mousemove(function (e) {
+$("body").mousemove(function (e) {
   var moveinX = (e.pageX * -1) / 100;
   var moveinY = (e.pageY * -1) / 100;
   $(this).css("background-position", moveinX + "px " + moveinY + "px ");
 });
-
-// var currentX = "";
-// var currentY = "";
-// var movementConstant = 0.025;
-
-// $(document).mousemove(function (e) {
-//   if (currentX == "") currentX = e.pageX;
-//   var xdiff = e.pageX - currentX;
-//   currentX = e.pageX;
-//   if (currentY == "") currentY = e.pageY;
-//   var ydiff = e.pageY - currentY;
-//   currentY = e.pageY;
-
-//   $(".parallax div").each(function (i, el) {
-//     var movement = (i + 1) * (xdiff * movementConstant);
-//     var movementy = (i + 1) * (ydiff * movementConstant);
-//     var newX = $(el).position().left + movement;
-//     var newY = $(el).position().top + movementy;
-//     var cssObj = {
-//       left: newX + "px",
-//       top: newY + "px",
-//     };
-
-//     // $(el).css('left', newX + 'px');
-//     // $(el).css('top', newY + 'px');
-//     $(el).css({
-//       transform: "translate(" + newX + "px," + newY + "px)",
-//     });
-//   });
-// });
