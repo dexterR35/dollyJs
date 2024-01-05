91c73b1e7c49d0b828a2474b75bfcc4a080ff7c2
dexterR35 <pusaw202@gmail.com>
2024-01-06 01:25:53 +0200

    update funct.

--- index2.js
+++ index2.js
@@ -55,42 +55,38 @@
 
   function e() {
     let t;
-
+    let distance = 1800;
     (t = "click" == n.touchClick ? 0 : 0.08),
       (n.dolly = new Dolly(document.getElementById("dolly_first"), {
         ease: t,
-        perspective: 2000,
-        distance: 1500,
-        fullpage: false,
+        perspective: 3000,
+        distance: distance,
+        fullpage: true,
         delay: 900,
-        duration: 700,
+        duration: 1000,
         onlyAnchors: !1,
         bodyClass: true,
         threshold: 10,
         activeMenu: !0,
-        onReady: function () {
-          /* CALLED WHEN PLUGIN IS READY */
-        },
+        onReady: function () {},
         onScroll: function (index, scroll, direction, limit) {
           // console.log(-scroll / 10);
           let zeus = document.querySelector(".contents");
 
-          let _cloud = document.querySelector(".sc-cove");
-          if (scroll < 2000) {
-            zeus.style.transition = "transform 0.2s linear";
-            zeus.style.transform = `translate3d(0%, 0%, ${scroll / 2}px)`;
-            // zeus.style.left = scroll * 0.2 + "px";
-
-            _cloud.style.transition = "transform 0.2s linear";
-            // _cloud.style.transform = `translate3d(0%, 0%, ${scroll * 21}px)`;
-            _cloud.style.top = scroll * 0.02 + "%";
-            console.log("50%" + (scroll * 0.2 + "%"));
+          let _towers = document.querySelector(".img-cover");
+          let _c_cloud = document.querySelector("._c_cloud");
+          let _c_logo = document.querySelector("._c_logo");
+          if (scroll < 3000) {
+            zeus.style.transition = "transform 0.05s linear";
+            zeus.style.transform = `translate3d(0%, 0%, ${scroll * 1.4}px)`;
+            _c_logo.style.transform = `translateZ(${scroll / 2}px)`;
+            _c_cloud.style.bottom = -scroll / 140 + "%";
+            _towers.style.transition = "transform 0.2s linear";
+            _towers.style.bottom = -scroll / 60 + "%";
           } else {
             // zeus.style.left = 0 + "px";
-            zeus.style.transform = `translate3d(0%, 0%, 0)`;
+            // zeus.style.transform = `translate3d(0%, 0%, 0)`;
           }
-
-          // Adjust the translateY based on scroll direction
         },
         onChange: function (prev, index, scroll) {
           // console.log("DollyJS scroll", scroll);
@@ -111,17 +107,6 @@
       for (let t = 0; t < n.mobileBtn.length; t++)
         n.mobileBtn[t].removeEventListener(n.touchClick, i);
   }
-  // function smoothScroll(elementSelector, translateY) {
-  //   let element = document.querySelector(elementSelector);
-  //   element.style.transition = "transform 0.5s ease";
-  //   element.style.transform = `translate3d(0%, 0%, ${translateY}px)`;
-
-  //   // Additional code for smooth scroll
-  //   element.scrollTo({
-  //     top: translateY,
-  //     behavior: "smooth",
-  //   });
-  // }
   document.addEventListener("DOMContentLoaded", function (t) {
     e();
   });
@@ -134,44 +119,22 @@
 //   $(this).css("background-attachment", "fixed");
 // });
 
-// var b = document.getElementsByTagName("BODY")[0];
+var b = document.getElementsByTagName("BODY")[0];
 
-// b.addEventListener("mousemove", function (event) {
-//   parallaxed(event);
-// });
+b.addEventListener("mousemove", function (event) {
+  parallaxed(event);
+});
 
-// function parallaxed(e) {
-//   var amountMovedX = (e.clientX * -0.3) / 30;
-//   var amountMovedY = (e.clientY * -0.3) / 30;
-//   var x = document.getElementsByClassName("parallaxed");
-//   var i;
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.transform =
-//       "translate(" + amountMovedX + "px," + amountMovedY + "px)";
-//   }
-// }
-
-// let header = document.querySelector(".symbol.logo");
-// let firstWoman = document.querySelector(".symbol.cloud");
-// let firstMan = document.querySelector(".yellow");
-// let secondWoman = document.querySelector(".blue");
-// // let escalator = document.getElementById("escalator");
-// let secondMan = document.querySelector(".symbol-center");
-// // console.log(header);
-// window.addEventListener("scroll", function () {
-//   let value = window.scrollY;
-//   console.log(value.toString());
-//   header.style.top = value * 0.5 + "px";
-//   firstWoman.style.top = value * 0.75 + "px";
-//   firstWoman.style.left = value * -0.5 + "px";
-//   firstMan.style.top = value * 0.25 + "px";
-//   firstMan.style.left = value * 0.5 + "px";
-//   secondWoman.style.top = value * 0.25 + "px";
-//   secondWoman.style.left = value * -0.5 + "px";
-//   // escalator.style.top = value * 0.5 + "px";
-//   secondMan.style.top = value * 0.35 + "px";
-//   secondMan.style.left = value * -0.25 + "px";
-// });
+function parallaxed(e) {
+  var amountMovedX = (e.clientX * -0.3) / 30;
+  var amountMovedY = (e.clientY * -0.3) / 30;
+  var x = document.getElementsByClassName("parallaxed");
+  var i;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.transform =
+      "translate(" + amountMovedX + "px," + amountMovedY + "px)";
+  }
+}
 
 // function toggleCustomDiv() {
 //   if ($("body").hasClass("on-examples")) {
